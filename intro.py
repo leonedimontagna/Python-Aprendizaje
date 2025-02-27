aadd0d7352094b769cd91ca3a7b40b638bfe71e9
Leonedimontagna <aitorruiz160@gmail.com>
2025-02-27 19:37:05 +0100

    Tipos

---
+++ intro.py
@@ -0,0 +1,8 @@
+"Introducción a Python."
+
+print("Hola Mundo!")
+print("El weta " * 4)
+# Para guardar lo que estamos haciendo, pulsamos las teclas ctrl seguido de s 'ctrl + s' para guardar el progreso.
+# Un aspecto importante, una vez abierta la terminal integrada, pulsamos flecha hacia arriba antes de ejecutar el código escrito. Nos cambia el punto blanco por una x, esa es la forma gráfica de visualizarlo.
+# Los programas que nosotros vamos a ejecutar en python se van a ejecutar desde arriba hacia abajo.
+# Utilizando el operador * seguido de un número '* 4' nos va a decir la cantidad de veces que queremos que se repita una palabra.
