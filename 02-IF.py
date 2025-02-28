a9e919e917aa12179e894ce6596114d57822c8de
Puma <aitorruiz160@gmail.com>
2025-02-28 12:27:16 +0100

    Add files via upload

---
+++ 02-IF.py
@@ -0,0 +1,14 @@
+edad = 75
+if edad >= 65:
+    print("Puede ver la película con super descuento")
+elif edad > 54:
+    print("Puedes ver la película con descuento")
+elif edad > 17:
+    print("Puedes ver la película")
+else:
+    print("No puedes entrar")
+
+print("Listo")
+
+# El orden en el cual nosotros ponemos nuestras expresiones es sumamente importante, porque si no es un error. Siempre va desde lo mayor hacia lo menor.
+
